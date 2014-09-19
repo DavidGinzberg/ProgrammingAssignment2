6a8b2deb6d90d10114999790a7e6bfeed654c394
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2014-09-19 16:48:00 -0400

    Implement makeCacheMatrix function
    Implemented the makeCacheMatrix function following in the style of the
    cached vector example.

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    ##By defualt, nothing is cached when the object is first created
+    inverse <- NULL
+    
+    ## Accessor and mutator functions for matrix value and cached inverse
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(i) inverse <<- i
+    getinverse <- function() inverse
+    
+    ## Return the list of accessors and mutators for this object
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
 
 }
 
